15b3d26e3d3f833a09666fad1dd094cc1f36e758
Aiysha Sadana <aiyshasadana@gmail.com>
2019-09-11 19:33:30 -0400

    updating weather.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,8 +9,9 @@ $(document).ready(function(){
                 url: 'http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial' + '&APPID=9b40e26591d4e0f348c8b86121312754',
                 type: 'GET',
                 dataType: 'jsonp',
-                success:function(data){
-                    console.log(data);
+                success:function(weather){
+                    console.log(weather)
+                    $('body').append("<p>" + weather)
                 }
             });
 
