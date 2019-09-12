275d9c781835dfba6b430925a7a35336fb388430
Aiysha Sadana <aiyshasadana@gmail.com>
2019-09-11 22:07:12 -0400

    pushing changes to weather branch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-    $('#btn-submit').click(function(){
-        var userInput = $('#user-input').val();
-
-        if (userInput != '') {
-
-            $.ajax({
-
-                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial' + '&APPID=9b40e26591d4e0f348c8b86121312754',
-                type: 'GET',
-                dataType: 'jsonp',
-                success:function(weather){
-                    console.log(weather)
-                    $('body').append("<p>" + weather)
-                }
-            });
-
-        } else{ ("#error").html ('Field cannot be empty');
-
-        }
-
+ 
+    $(document).ready(function(){
+        $('#btn-submit').click(function(){
+            var userInput = $('#user-input').val();
+     
+            if (userInput != '') {
+     
+                $.ajax({
+     
+                    url: 'http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial' + '&APPID=9b40e26591d4e0f348c8b86121312754',
+                    type: 'GET',
+                    dataType: 'jsonp',
+                    success:function(weather){
+                        console.log(weather)
+                        $('body').append(`<p>Temperature: ${weather.main.temp}`)
+                    }
+                });
+     
+            } else{ ("#error").html ('Field cannot be empty');
+     
+            }
+     
+        });
+     
     });
-
-});
-
 
 
     
