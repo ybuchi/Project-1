7bdac4ab39f25a73a1645e8e25e0d84b4733ef6f
William Mooney Sloneker <wmsloneker@gmail.com>
2019-09-12 21:14:10 -0400

    Repaired Google-Maps logic and testPage.html structure

--- assets/javascript/google-maps.js
+++ assets/javascript/google-maps.js
@@ -7,7 +7,7 @@ var service;
 var infowindow;
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map-api'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: { lat: 41.323516, lng: -71.804009 },
     //Control Features for the map Display
@@ -21,6 +21,7 @@ function initMap() {
 
   //Transit Layers
   var geocoder = new google.maps.Geocoder();
+
   var trafficLayer = new google.maps.TrafficLayer();
   trafficLayer.setMap(map);
   var transitLayer = new google.maps.TransitLayer();
@@ -28,56 +29,53 @@ function initMap() {
   var bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
 
-  // //Listener for new user input
-  // document.getElementById('btn-submit').addEventListener('click', function () {
-  //   geocodeAddress(geocoder, map);
-  // });
+  //Listener for new user input
+  document.getElementById('btn-submit').addEventListener('click', function () {
+    geocodeAddress(geocoder, map);
+  });
 }
 
 // Function that receives an address from the user and receives polar coordinates for the location from Google Maps
 var address = document.getElementById('address').value;
 
 function geocodeAddress(geocoder, resultsMap) {
-
+  var address = document.getElementById('address').value;
   geocoder.geocode({ 'address': address }, function (results, status) {
     if (status === 'OK') {
-      console.log(results[0]);
       resultsMap.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
         map: resultsMap,
         position: results[0].geometry.location
       });
-
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
-
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        var place = results[i];
-        createMarker(results[i]);
-      }
-    }
-  }
-
 }
 
-Nearby Places Request
-var request = {
-  //Need to figure out a way to get Long/Lat into this object field
-  location: getLocation(address),
-  radius: '16129',
-  keyword: 'beach'
-};
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
 
-console.log(request.location);
+//Nearby Places Request
+// var request = {
+//   //Need to figure out a way to get Long/Lat into this object field
+//   location: getLocation(address),
+//   radius: '16129',
+//   keyword: 'beach'
+// };
 
-service = new google.maps.places.PlacesService(map);
-service.nearbySearch(request, callback);
+// console.log(request.location);
 
-Ajax Request to Geocode API
+// service = new google.maps.places.PlacesService(map);
+// service.nearbySearch(request, callback);
+
+//Ajax Request to Geocode API
 var getLocation = function (address) {
   $.ajax({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyD_uRt9vKnbfDV5hnOzck20BCb6-NhR0kc',
